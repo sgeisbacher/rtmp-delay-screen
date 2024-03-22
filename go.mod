090e3fb74e5a9773cf75587dcb3df9e952b814cc
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2024-03-22 11:32:08 +0100

    set up Sean-Der's rtmp-to-webrtc with go-templ

---
+++ go.mod
@@ -0,0 +1,42 @@
+module github.com/sgeisbacher/go-rtmp-delay
+
+go 1.22.0
+
+require (
+	github.com/a-h/templ v0.2.598
+	github.com/pion/webrtc/v3 v3.2.28
+	github.com/pkg/errors v0.9.1
+	github.com/yutopp/go-flv v0.3.1
+	github.com/yutopp/go-rtmp v0.0.0-20191212152852-4e41609a99bb
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pion/datachannel v1.5.5 // indirect
+	github.com/pion/dtls/v2 v2.2.7 // indirect
+	github.com/pion/ice/v2 v2.3.13 // indirect
+	github.com/pion/interceptor v0.1.25 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.12 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.12 // indirect
+	github.com/pion/rtp v1.8.3 // indirect
+	github.com/pion/sctp v1.8.12 // indirect
+	github.com/pion/sdp/v3 v3.0.6 // indirect
+	github.com/pion/srtp/v2 v2.0.18 // indirect
+	github.com/pion/stun v0.6.1 // indirect
+	github.com/pion/transport/v2 v2.2.3 // indirect
+	github.com/pion/turn/v2 v2.1.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/yutopp/go-amf0 v0.1.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/pion/webrtc/v3"
+	"github.com/sgeisbacher/go-rtmp-delay/ui"
+)
+
+func main() {
+	http.Handle("/", templ.Handler(ui.PlayerLayout()))
+	http.HandleFunc("/createPeerConnection", createPeerConnection)
+
+	fmt.Println("Listening on :8080")
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+// Add a single video track
+func createPeerConnection(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Incoming HTTP Request")
+
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		panic(err)
+	}
+
+	videoTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264}, "video", "pion")
+	if err != nil {
+		panic(err)
+	}
+	if _, err = peerConnection.AddTrack(videoTrack); err != nil {
+		panic(err)
+	}
+
+	audioTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypePCMA}, "audio", "pion")
+	if err != nil {
+		panic(err)
+	}
+	if _, err = peerConnection.AddTrack(audioTrack); err != nil {
+		panic(err)
+	}
+
+	var offer webrtc.SessionDescription
+	if err := json.NewDecoder(r.Body).Decode(&offer); err != nil {
+		panic(err)
+	}
+
+	if err := peerConnection.SetRemoteDescription(offer); err != nil {
+		panic(err)
+	}
+
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	} else if err = peerConnection.SetLocalDescription(answer); err != nil {
+		panic(err)
+	}
+	<-gatherComplete
+
+	response, err := json.Marshal(peerConnection.LocalDescription())
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(response); err != nil {
+		panic(err)
+	}
+
+	go startRTMPServer(peerConnection, videoTrack, audioTrack)
+}
